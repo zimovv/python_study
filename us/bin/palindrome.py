aa67df88be526da8b39837ed0eff9a0b4ed2e065
zimovv <781422258@qq.com>
2016-08-01 17:16:58 +0800

    Create palindrome.py
    Check if palindrome.

---
+++ us/bin/palindrome.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import sys
+lst = ['Radar', 'Lemon', 'aka', 'Do geese see God']
+
+def is_palindrome(letters):
+    #TODO: implement the algorithm
+    pass
+
+
+if __name__ == '__main__':
+    #TODO: Use built-in functions map or filter to filter out the palindromes
+    #it = map(...)
+    #it = filter(...)
+
+    #Print palindroms
+    for i in it:
+        print i
