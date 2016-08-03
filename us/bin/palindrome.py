182024a35027bd31327814a13a689fe3af3a03d6
zimovv <781422258@qq.com>
2016-08-03 17:30:04 +0800

    Update palindrome.py

--- us/bin/palindrome.py
+++ us/bin/palindrome.py
@@ -4,14 +4,20 @@ lst = ['Radar', 'Lemon', 'aka', 'Do geese see God']
 
 def is_palindrome(letters):
     #TODO: implement the algorithm
-    pass
+    letters = ''.join(letters.split())
+    letters_v = letters[::-1]
+    if letters_v.lower() == letters.lower():
+        return letters
 
 
 if __name__ == '__main__':
     #TODO: Use built-in functions map or filter to filter out the palindromes
     #it = map(...)
     #it = filter(...)
-
+    
+    #it = map(is_palindrome, lst)  #This will give out redundant 'None'
+    it = filter(is_palindrome, lst)
+    
     #Print palindroms
     for i in it:
         print i
